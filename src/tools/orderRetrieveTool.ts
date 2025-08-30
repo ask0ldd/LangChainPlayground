960a44ff273fe1230ce9ddf9a8cacddcacefb946
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-08-30 12:35:42 +0200

    *edit* front / updated return order datas

--- src/tools/orderRetrieveTool.ts
+++ src/tools/orderRetrieveTool.ts
@@ -4,11 +4,13 @@ import orderRetriveInputSchema from "./schemas/orderRetrieveInputSchema";
 const orderRetrieveTool = tool(
     async ({customerName, orderId}) => {
         if(orderId) {
+            console.log('orderId :', orderId)
             return ({id : orderId, product : '3 black shirts', customer : 'ced', date : Date.now})
         }
 
         if(customerName?.toLowerCase().includes("ced")) {
-            return ({id : 123456, product : '2 cargo shorts', customer : 'ced', date : Date.now})
+            console.log('customerName :', customerName)
+            return ({id : 123456, product : '2 cargo shorts', customer : 'ced', date : Date.now, status : 'Payment has been rejected due to insufficient funds on the cutsomer account.'})
         }
 
         return "no result"
